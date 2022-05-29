6dc7ca31281efc189be646f99f63e01140921232
Gautham Venkataraman <gauthsvenkat99@gmail.com>
2022-05-29 16:51:02 +0200

    Cesar metrics (#6)
    * some more metrics
    
    * fstrings don't work in python 3.5 so adjusted them.. Bug in line 227 of evaluate
    
    * debugged metrics
    
    * Revert "Merge branch 'feature/feature_uniqueness' into cesar_metrics"
    
    This reverts commit d0788c67a8d551c58ed3204c591513f4007d030d, reversing
    changes made to e9ed609a030748418111275df1235a15c972547f.
    
    Co-authored-by: Cesar <cesarvdpoel@gmail.com>
    Co-authored-by: Cesar48 <43107266+Cesar48@users.noreply.github.com>

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import gym
 import numpy as np
+import math
 
 from src.DTPolicy import DTPolicy
 from src.QLearning import QLearning
@@ -111,6 +112,7 @@ class Evaluate:
 
             #Captures repetitive use of the same features.
             feature_uniqueness_ratio = []
+
             for i in range(len(decision_paths)):
                 features.append({}) #for each observation track the list of features encountered when making the decision
                 for j in range(len(decision_paths[i])-1):
@@ -121,25 +123,9 @@ class Evaluate:
                         features[i][feature] += 1
 
                 #return the number of unique features used/ total number of features used for obtaining the action for an observation
-
-                """If the counts for two nodes are 10000 and 2, then effectively its as if there is only 1 node being used.
-                The below function gets this effective node usage count. Earlier method using .keys() would have resulted
-                in 2 effective nodes. We need something in between 1 an 2"""
-                equi_nodes = self.getEffectiveFeaturesUsed(features[i].values())
-                feature_uniqueness_ratio.append(equi_nodes/depths_mod[i])
-
+                feature_uniqueness_ratio.append(len(features[i].keys())/depths_mod[i])
 
             print('{} expected uniqueness ratio {}'.format(name, np.mean(feature_uniqueness_ratio)))
-
-    def getEffectiveFeaturesUsed(self, usage_counts):
-        """Example: Assume there are two nodes with occurrence counts 10000 and 2. 1/np.mean() results in 1/5001. Now arr becomes [10000,2] * 1/5001
-         =  [1.99,3.99 x 10^-4]. Effectively only the first one should be counted. So, we threshold values to 1 then sum to get the effective count"""
-        if len(usage_counts) == 0:
-             return 0
-        arr = np.array(list(usage_counts))
-        arr = arr * (1.0/np.mean(arr)) #to see which values exceed the average.
-        arr = np.clip(arr, 0, 1) #All nodes with values greater than the mean are counted as effective. So threshold at 1.
-        return np.sum(arr)
 
     def node_counts(self):
         #simple metric. Returns the number of nodes in the tree
@@ -162,7 +148,115 @@ class Evaluate:
                 completeness_ratio = 1.0
             print("{} completeness ratio: {}".format(name,completeness_ratio))
 
+    def feature_importance_score(self):
+        # It's better to have a few important features and not or barely use the others, users only need to keep the important ones in mind
+        for name, policy in zip(self.policy_names, self.policies):
+            # if there are little important features, 1-(importance) will have mostly high values so their product will be high
+            try:
+                fis = np.prod(1-policy.tree.tree_.compute_feature_importances())
+            except AttributeError:
+                fis = 0
+            print("{} has importance score {}".format(name, fis))
 
+    def insignificant_leaves(self):
+        for name, policy in zip(self.policy_names, self.policies):
+            try:
+                max_sample = policy.tree.n_node_samples[0]
+                non_leaves = policy.tree.tree_.children_left != -1 != policy.tree.tree_.children_right
+                useless = policy.tree.n_node_samples[non_leaves] < max_sample * 0.01  # splits on less than 1 percent of samples are annoying
+            except AttributeError:
+                useless = 0
+                non_leaves = [1]
+            print("{} has {}% insignificant splits".format(name, 100 * useless / np.sum(non_leaves)))
+
+    def exact_feature_uniqueness(self):
+        for name, policy in zip(self.policy_names, self.policies):
+            try:
+                paths = [[0]]
+                uniques = []
+                while paths:
+                    cur = paths.pop()
+                    l = policy.tree.tree_.children_left[cur[-1]]
+                    r = policy.tree.tree_.children_right[cur[-1]]
+                    if (l == -1 == r):
+                        uniques.append(np.unique(cur).size / cur.size)
+                    else:
+                        cur_ = cur.copy()
+                        cur.append(r)
+                        paths.append(cur)
+
+                        cur_.append(l)
+                        paths.append(cur_)
+            except AttributeError:
+                uniques = [1]
+            print("{} had {}% unique features in each path".format(name, np.average(uniques) * 100))
+
+    def unnecessary_splits(self):
+        for name, policy in zip(self.policy_names, self.policies):
+            try:
+                print("{} could prune {}% of nodes without reducing performance".format(name, 100 * self._unnecessary_splits(policy, 0)[2] / policy.tree.tree_.node_count))
+            except AttributeError:
+                print("{} couldn't prune".format(name))
+
+            
+    def _unnecessary_splits(self, policy, node):
+        l = policy.tree.tree_.children_left[node]
+        r = policy.tree.tree_.children_right[node]
+        if l == -1 == r:
+            return True, policy.tree.tree_.value[node].argmax(), 0
+        lu,lc,ln = self._unnecessary_splits(policy, l)
+        ru,rc,rn = self._unnecessary_splits(policy, r)
+
+        if not ru or not lu or lc != rc:
+            return False, None, ln+rn
+
+        return True, lc, ln+rn+1
+
+    def same_feature_value_differences(self):
+        for name, policy in zip(self.policy_names, self.policies):
+            try:
+                features = policy.tree.tree_.feature[policy.tree.tree_.feature >= 0]
+                stds = []
+                for f in np.unique(features):
+                    values = policy.tree.tree_.threshold[policy.tree.tree_.feature == f]
+                    if values.size > 1:
+                        stds.append(np.std(values))
+            except AttributeError:
+                stds = [0]
+            if len(stds) == 0:
+                print("{} has no repeating features".format(name))
+            else:
+                print("{} has an average standard deviation of {} among repeating features (max {}, min {})".format(name, np.average(stds), np.max(stds), np.min(stds)))
+
+    def same_value_differences_in_path(self):
+        for name, policy in zip(self.policy_names, self.policies):
+            paths = [[0]]
+            stds = []
+            try:
+                while paths:
+                    cur = paths.pop()
+                    l = policy.tree.tree_.children_left[cur[-1]]
+                    r = policy.tree.tree_.children_right[cur[-1]]
+                    if (l == -1 == r):
+                        _features = policy.tree.tree_.feature[cur]
+                        features = policy.tree.tree_.feature[policy.tree.tree_.feature >= 0]
+                        for f in np.unique(features):
+                            values = policy.tree.tree_.threshold[policy.tree.tree_.feature == f]
+                            if values.size > 1:
+                                stds.append(np.std(values))
+                    else:
+                        cur_ = cur.copy()
+                        cur.append(r)
+                        paths.append(cur)
+
+                        cur_.append(l)
+                        paths.append(cur_)
+            except AttributeError:
+                stds = [0]
+            if len(stds) == 0:
+                print("{} has no repeating features".format(name))
+            else:
+                print("{} has an average standard deviation of {} among repeating features in a path (max {}, min {})".format(name, np.average(stds), np.max(stds), np.min(stds)))
 
 
     def evaluate(self):
@@ -172,3 +266,9 @@ class Evaluate:
         self.feature_uniqueness()
         self.node_counts()
         self.tree_completeness_ratio()
+        self.feature_importance_score()
+        self.insignificant_leaves()
+        self.exact_feature_uniqueness()
+        self.unnecessary_splits()
+        self.same_feature_value_differences()
+        self.same_value_differences_in_path()
