2609b727225567a30851f6d71bc81c2db1b68dfb
Gautham Venkataraman <gauthsvenkat99@gmail.com>
2022-06-03 15:04:32 +0200

    added some extra dependencies for docker runtime environment

---
+++ scripts/run_docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Launch an experiment using the docker cpu image
+# Only works by default for mac and linux
+
+JULIA_CMD=julia
+DOCKER_IMAGE=iai
+
+# export IAI_LICENSE_FILE=iai-config/iai-server.lic
+# nohup "$JULIA_CMD" --sysimage="$FILE" -e "IAI.IAILicensing.start_server(); wait(Condition())" > /dev/null 2>&1 &
+# SERVER_PID=$!
+
+CMD_LINE="$@"
+
+echo "Executing in the docker (cpu image):"
+echo $CMD_LINE
+
+docker run -it --network host --ipc=host -p 8888:8888 \
+ --mount src=$(pwd),target=/root/code/rl_zoo,type=bind $DOCKER_IMAGE\
+  bash -c "cd /root/code/rl_zoo/ && $CMD_LINE"
+
+# kill $SERVER_PID
