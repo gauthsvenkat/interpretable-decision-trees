382ab5d9c90836bc311ce803f495089bb671a4da
adis98 <aditya.ssr@gmail.com>
2022-06-11 17:17:09 +0200

    Fix to experiment runner

--- experiment_runner.bat
+++ experiment_runner.bat
@@ -1,14 +1,14 @@
 FOR %%G in (Acrobot-v1, CartPole-v1, MountainCar-v0) DO (
     FOR %%H in (1,2,3,4,5) DO (
         FOR %%J in (0.0,0.5,1.0) DO (
-            py -W ignore -m src.feature_tester --env_name %%G --max_depth %%H --student_path _depth_%%H --bc_path _depth_%%H --experiment %%G_depth_%%H --rollouts 100 --no_print true --cp %%J --optimal
+            py -W ignore -m src.main --env_name %%G --max_depth %%H --student_path _depth_%%H --bc_path _depth_%%H --experiment %%G_depth_%%H --rollouts 100 --no_print true --cp %%J --optimal
         )
     )
 )
 
 FOR %%G in (Acrobot-v1, CartPole-v1, MountainCar-v0) DO (
     FOR %%H in (1,2,3,4,5) DO (
-        py -W ignore -m src.feature_tester --env_name %%G --max_depth %%H --student_path _depth_%%H --bc_path _depth_%%H --experiment %%G_depth_%%H --rollouts 100 --no_print true
+        py -W ignore -m src.main --env_name %%G --max_depth %%H --student_path _depth_%%H --bc_path _depth_%%H --experiment %%G_depth_%%H --rollouts 100 --no_print true
     )
 )
 
